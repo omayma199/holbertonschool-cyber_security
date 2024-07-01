f81cab40f75f008f4d7ddb0c722f1e33b016e0be
omayma19 <5752@holbertonstudents.com>
2024-07-01 19:24:02 +0100

    intro

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-distributor_id=$(lsb_release -i | awk -F':' '{print $2}' | sed 's/^[ \t]*//'); echo "Distributor ID: $distributor_id"
+distributor_id=$(lsb_release -i | awk -F':' '{print $2}' | sed 's/^[ \t]*//'); { echo "Distributor ID: $distributor_id"; } > temp.txt && cat temp.txt && rm temp.txt
