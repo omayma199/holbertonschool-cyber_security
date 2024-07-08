dc26a16970bb288e70f6a8d6ce447b134ec27cae
omayma19 <5752@holbertonstudents.com>
2024-07-08 10:42:31 +0100

    test

--- cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/0-release.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsb_release -i | awk -F':' '{print "Distributor ID:" $2}' | sed 's/^[ \t]*//'
+lsb_release -s -i | tr -d '\n'
--- cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
+++ cybersecurity_basics/0x00_introduction_cybersecurity/2-sha256_validator.sh
@@ -1,23 +1,2 @@
 #!/bin/bash
-
-# Check if the user provided a key name
-if [ -z "$1" ]; then
-  echo "Usage: $0 <key-name>"
-  exit 1
-fi
-
-# Set the key name
-KEY_NAME=$1
-
-# Generate the RSA SSH key pair
-ssh-keygen -t rsa -b 4096 -f "$KEY_NAME" -N ""
-
-# Check if the key generation was successful
-if [ $? -eq 0 ]; then
-  echo "RSA SSH key pair generated successfully."
-  echo "Private key: $KEY_NAME"
-  echo "Public key: $KEY_NAME.pub"
-else
-  echo "An error occurred during key generation."
-  exit 1
-fi
+sha256sum -c <(echo "$1  $2")
