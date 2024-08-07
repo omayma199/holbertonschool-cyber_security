74150e31fc9916aedfdf918f21d218c631efc615
omayma199 <5752@holbertonstudents.com>
2024-08-07 12:58:10 +0100

    test

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -3,5 +3,5 @@
 # Define the log file path
 LOG_FILE="logs.txt"
 
-# Extract IP addresses, count their occurrences, and sort by frequency
-awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 1
+# Extract IP addresses from the log file, count their occurrences, and sort by frequency
+awk '{print $1}' "$LOG_FILE" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq -c | sort -nr | head -n 1
