95819ad902c083d636f991f9b0fc55ad014eaf65
omayma199 <5752@holbertonstudents.com>
2024-08-07 14:37:34 +0100

    test

--- web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/3-library.sh
@@ -6,5 +6,5 @@ LOG_FILE="logs.txt"
 # Find the IP address with the most requests
 ATTACKER_IP=$(awk '{print $1}' "$LOG_FILE" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq -c | sort -nr | head -n 1 | awk '{print $2}')
 
-# Extract user-agent strings for the attacker's IP address
-grep "$ATTACKER_IP" "$LOG_FILE" | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}'
+# Extract user-agent strings for the attacker's IP address and find the most frequent one
+grep "$ATTACKER_IP" "$LOG_FILE" | awk -F'"' '{print $6}' | sort | uniq -c | sort -nr | head -n 1 | awk '{$1=""; print $0}' | sed 's/^ *//; s/ *$//'
