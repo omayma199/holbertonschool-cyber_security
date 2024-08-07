33fe747e6a278079c920503e2702302f8a2e0177
omayma199 <5752@holbertonstudents.com>
2024-08-07 13:13:31 +0100

    test

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -4,4 +4,4 @@
 LOG_FILE="logs.txt"
 
 # Extract IP addresses, count their occurrences, sort by frequency, and print only the IP address
-awk '{print $1}' "$LOG_FILE" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq -c | sort -nr | awk '{print $2}' | head -n 1
+sudo awk '{print $1}' "$LOG_FILE" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq -c | sort -nr | awk '{print $2}' | head -n 1
