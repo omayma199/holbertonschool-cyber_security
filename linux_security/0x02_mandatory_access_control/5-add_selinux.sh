8c77a93254d05f26355848b17b6e205ee5222b0a
root <root@DESKTOP-442GGH3.>
2024-08-14 12:39:24 +0100

    test

--- linux_security/0x02_mandatory_access_control/5-add_selinux.sh
+++ linux_security/0x02_mandatory_access_control/5-add_selinux.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-if [ -z "$1" ]; then
-    echo "Usage: $0 <new_login_name>"
-    exit 1
-fi
-sudo semanage login -a -s user_u "$1"
+[ -z "$1" ] && echo "Usage: $0 <new_login_name>" && exit 1; sudo semanage login -a -s user_u "$1"
