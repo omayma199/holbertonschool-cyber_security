b808497561eeade87ab525ccbfd14cfd41c8c2ce
root <root@DESKTOP-442GGH3.>
2024-11-19 10:16:34 +0100

    test

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Define the log file path
-LOG_FILE="logs.txt"
-
 # Extract IP addresses, count their occurrences, sort by frequency, and print only the IP address
-awk '{print $1}' "$LOG_FILE" | grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' | sort | uniq -c | sort -nr | awk '{print $2}' | head -n 1
+grep -Eo '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' logs.txt | sort | uniq -c | sort -nr | awk '{print $2}' | head -n 1
