987dbcc650bc1a9421d84225b0a7d736b19dcec9
omayma19 <5752@holbertonstudents.com>
2025-01-21 16:42:28 +0100

    test

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
+
+
 """Documentation :
 Module docs.
 """
+
 import sys
 import os
+
+
 def usage():
     """Documentation :
     usage function docs.
     """
     print("Usage: read_write_heap.py pid search_string replace_string")
     sys.exit(1)
+
+
 def main():
     """Documentation :
     main function docs.
     """
     if len(sys.argv) != 4:
         usage()
+
     pid = int(sys.argv[1])
     search_string = sys.argv[2].encode()
     replace_string = sys.argv[3].encode()
+
     mem_path = f"/proc/{pid}/mem"
     maps_path = f"/proc/{pid}/maps"
+
     try:
         with open(maps_path, 'r') as maps_file:
             for line in maps_file.readlines():
@@ -28,10 +38,12 @@ def main():
                 start = int(parts[0].split('-')[0], 16)
                 end = int(parts[0].split('-')[1], 16)
                 permissions = parts[1]
+
                 if 'heap' in line and 'rw-p' in permissions:
                     with open(mem_path, 'r+b') as mem_file:
                         mem_file.seek(start)
                         data = mem_file.read(end - start)
+
                         index = data.find(search_string)
                         if index != -1:
                             new_1 = data[:index] + replace_string
@@ -44,5 +56,7 @@ def main():
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
+
+
 if __name__ == "__main__":
     main()
