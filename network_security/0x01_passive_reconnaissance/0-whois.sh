192dcae84b284822f7ffaaadf626edd70ce790c1
omayma19 <5752@holbertonstudents.com>
2025-09-05 16:13:46 +0100

    test

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/Registrant|Admin|Tech/ && $1 !~ /Registry/ {gsub(/^ +| +$/,"",$2); if($1 ~ /Street$/) $2=$2" "; if($1 ~ /Phone Ext|Fax Ext/) $2=":"; print $1","$2}' > "$1.csv"
+whois "$1" | awk -F: '/Registrant|Admin|Tech/ && $1 !~ /Registry/ {gsub(/^ +| +$/,"",$2); if($1 ~ /Street$/) $2=$2" "; print $1","$2} END {print "Registrant Phone Ext:,"; print "Registrant Fax Ext:,"; print "Admin Phone Ext:,"; print "Admin Fax Ext:,"; print "Tech Phone Ext:,"; print "Tech Fax Ext:,"}' > "$1.csv"
